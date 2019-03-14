8f73b281f8da9544824ff45643deba86ed19ba6d
Alex Cooper <micheloshake@gmail.com>
2019-03-13 22:57:37 -0300

    Platform name abbreviator

---
+++ src/helpers/platformNames.ts
@@ -0,0 +1,217 @@
+export const PlatformAbbreviator = (name: string) => {
+  switch (name) {
+    case 'Xbox': {
+      return 'XBOX';
+    }
+    case 'Xbox 360': {
+      return 'X360';
+    }
+    case 'Xbox One': {
+      return 'XBONE';
+    }
+    case 'Xbox Live Arcade': {
+      return 'XLA';
+    }
+    case 'PlayStation': {
+      return 'PSX';
+    }
+    case 'PlayStation 2': {
+      return 'PS2';
+    }
+    case 'PlayStation 3': {
+      return 'PS3';
+    }
+    case 'PlayStation 4': {
+      return 'PS4';
+    }
+    case 'PlayStation 5': {
+      return 'PS5';
+    }
+    case 'PlayStation Portable': {
+      return 'PSP';
+    }
+    case 'PlayStation Vita': {
+      return 'PSVita';
+    }
+    case 'PlayStation Network': {
+      return 'PSN';
+    }
+    case 'Nintendo Entertainment System (NES)': {
+      return 'NES';
+    }
+    case 'Super Nintendo Entertainment System (SNES)': {
+      return 'SNES';
+    }
+    case 'Nintendo 64': {
+      return 'N64';
+    }
+    case 'Nintendo GameCube': {
+      return 'NGC';
+    }
+    case 'Wii': {
+      return 'Wii';
+    }
+    case 'Wii U': {
+      return 'WiiU';
+    }
+    case 'Nintendo Switch': {
+      return 'Switch';
+    }
+    case 'Game Boy': {
+      return 'GB';
+    }
+    case 'Game Boy Color': {
+      return 'GBC';
+    }
+    case 'Virtual Boy': {
+      return 'NVB';
+    }
+    case 'Game Boy Advance': {
+      return 'GBA';
+    }
+    case 'Nintendo DS': {
+      return 'NDS';
+    }
+    case 'Nintendo DSi': {
+      return 'NDSi';
+    }
+    case 'Nintendo 3DS': {
+      return '3DS';
+    }
+    case 'New Nintendo 3DS': {
+      return 'N3DS';
+    }
+    case 'Virtual Console (Nintendo)': {
+      return 'VCN';
+    }
+    case 'SG-1000': {
+      return 'S1000';
+    }
+    case 'Sega Master System': {
+      return 'SMS';
+    }
+    case 'Sega Mega Drive/Genesis': {
+      return 'Genesis';
+    }
+    case 'Sega CD': {
+      return 'SCD';
+    }
+    case 'Sega 32X': {
+      return '32X';
+    }
+    case 'Sega Saturn': {
+      return 'Saturn';
+    }
+    case 'Dreamcast': {
+      return 'SDC';
+    }
+    case 'Sega Game Gear': {
+      return 'SGG';
+    }
+    case 'Atari 2600': {
+      return '2600';
+    }
+    case 'Atari 5200': {
+      return '5200';
+    }
+    case 'Atari 7800': {
+      return '7800';
+    }
+    case 'Atari Jaguar': {
+      return 'Jaguar';
+    }
+    case 'Atari Lynx': {
+      return 'Lynx';
+    }
+    case 'PC (Microsoft Windows)': {
+      return 'PC';
+    }
+    case 'Mac': {
+      return 'OSX';
+    }
+    case 'Linux': {
+      return 'Linux';
+    }
+    case 'Neo Geo MVS': {
+      return 'NeoGeo';
+    }
+    case 'Neo Geo Pocket': {
+      return 'NGPocket';
+    }
+    case 'PC Engine SuperGrafx': {
+      return 'PCEngine';
+    }
+    case 'TurboGrafx-16/PC Engine': {
+      return 'TurboGrafx';
+    }
+    case 'Amiga': {
+      return 'Amiga';
+    }
+    case 'Amiga CD32': {
+      return 'AmigaCD32';
+    }
+    case '3DO Interactive Multiplayer': {
+      return '3DO';
+    }
+    case 'Philips CD-i': {
+      return 'CD-i';
+    }
+    case 'Vectrex': {
+      return 'Vectrex';
+    }
+    case 'MSX': {
+      return 'MSX';
+    }
+    case 'MSX2': {
+      return 'MSX2';
+    }
+    case 'ColecoVision': {
+      return 'CVision';
+    }
+    case 'Intellivision': {
+      return 'Intvis';
+    }
+    case 'ZX Spectrum': {
+      return 'ZXS';
+    }
+    case 'PC DOS': {
+      return 'DOS';
+    }
+    case 'Apple II': {
+      return 'Apple II';
+    }
+    case 'Odyssey': {
+      return 'Odyssey';
+    }
+    case 'Commodore VIC-20': {
+      return 'VIC20';
+    }
+    case 'Commodore 16': {
+      return 'C16';
+    }
+    case 'Commodore C64/128': {
+      return 'C64';
+    }
+    case 'Commodore Plus/4': {
+      return 'C.Plus';
+    }
+    case 'iOS': {
+      return 'iOS';
+    }
+    case 'Android': {
+      return 'Android';
+    }
+    case 'Mobile': {
+      return 'MOB';
+    }
+    case 'WonderSwan': {
+      return 'WS';
+    }
+    case 'WonderSwan Color': {
+      return 'WSC';
+    }
+    default: {
+      return 'OTHER';
+    }
+  }
+};
