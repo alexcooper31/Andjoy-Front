561590767eff15f983d1983397ce6fa19a0b174a
Alex Cooper <micheloshake@gmail.com>
2019-03-13 22:50:39 -0300

    Sample Env

---
+++ env.sample.ts
@@ -0,0 +1,12 @@
+const ENV = {
+  dev: {
+    apiUrl: 'http://localhost:3000/graphql',
+    igdbApiKey: '2333kj4hh5553kj5h54kl4k5h6j6h6l45',
+  },
+  prod: {
+    apiUrl: 'http://productionhost:3000/graphql',
+    igdbApiKey: 'fhhj4h43jh54jh54j6h66j6764j54jh43j34',
+  },
+};
+
+export default ENV;
